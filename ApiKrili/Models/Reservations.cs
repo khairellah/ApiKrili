4a7b07db773b29298142cf1edbe94f2b95cf7bae
khairellah <zakariakhairellah@gmail.com>
2022-01-11 15:36:20 +0100

    add Models

---
+++ ApiKrili/Models/Reservations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiKrili.Models
+{
+    public class Reservations
+    {
+        public int id_reservation { get; set; }
+        public int id_car { get; set; }
+        public int id_visitor { get; set; }
+        public int id_user { get; set; }
+        public String visitor_phone_number { get; set; }
+        public String visitor_img_name { get; set; }
+        public String visitor_lname { get; set; }
+        public int nbr_days { get; set; }
+        public DateTime date_reservation { get; set; }
+        public String review { get; set; }    
+
+        public Reservations() { }
+
+        public Reservations(int id_reservation, int id_car, int id_visitor, int id_user, string visitor_phone_number, string visitor_img_name, string visitor_lname, int nbr_days,DateTime date_reservation, string review)
+        {
+            this.id_reservation = id_reservation;
+            this.id_car = id_car;
+            this.id_user = id_user;
+            this.id_visitor = id_visitor;
+            this.date_reservation = date_reservation;
+            this.visitor_img_name = visitor_img_name;
+            this.visitor_lname = visitor_lname;
+            this.visitor_phone_number = visitor_phone_number;
+            this.review = review;
+            this.nbr_days = nbr_days;
+        }
+    }
+}
---
+++ ApiKrili/Models/Visitors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiKrili.Models
+{
+    public class Visitors
+    {
+        public int id_visitor { get; set; }
+        public String login { get; set; }
+        public String pass { get; set; }
+        public String fname { get; set; }
+        public String lname { get; set; }
+        public String phone_number { get; set; }
+        public String city { get; set; }
+        public String img_name { get; set; }
+        public int deleted { get; set; }
+
+        public Visitors() { }
+
+        public Visitors(int id_visitor, String login, String pass, string fname, string lname, string phone_number, string city, string img_name)
+        {
+            this.id_visitor = id_visitor;
+            this.login = login;
+            this.pass = pass;
+            this.fname = fname;
+            this.lname = lname;
+            this.phone_number = phone_number;
+            this.city = city;
+            this.img_name = img_name;
+        }
+    }
+}
